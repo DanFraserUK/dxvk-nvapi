dafdbda0fa00fa1dba324cfbfb25500130ff06b4
DanFraser <11152006+DanFraserUK@users.noreply.github.com>
2026-07-17 16:15:28 +0100

    SMP-DIAG-OUTCALLER logging

--- src/nvapi_d3d.cpp
+++ src/nvapi_d3d.cpp
@@ -479,6 +479,8 @@ NVAPI_FUNCTION NvAPI_D3D_SetMultiViewMode(IUnknown* pDevOrContext, NV_MULTIVIEW_
     if (pMultiViewParams->numViews == 0 || pMultiViewParams->numViews > NV_MULTIVIEW_MAX_SUPPORTED_VIEWS)
         return InvalidArgument(n);
 
+    log::info(str::format("[SMP-DIAG-OUTERCALLER] retaddr0=", log::fmt::ptr(__builtin_return_address(0)), " retaddr1=", log::fmt::ptr(__builtin_return_address(1))));
+
     // Phase 2: forward the toggle to an SMP-capable DXVK when present
     if (auto device = NvapiD3d11Device::GetOrCreate(pDevOrContext);
         device && device->SetMultiviewMode(pMultiViewParams->numViews, pMultiViewParams->independentViewportMaskEnable != 0))
